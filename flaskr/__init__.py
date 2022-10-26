2cd3b4b161a4ebdde509319d39f2cbb6cb9a1bd1
Juan <juan.ballesteros@u.icesi.edu.co>
2022-10-26 17:50:31 -0500

    allow cors

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,7 +6,7 @@ from flask import abort
 from flask import request
 from flask import jsonify
 from werkzeug.exceptions import HTTPException
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flaskr.services.functions import upload_file_service, get_files_service, get_storage_service, get_host_service
 
 # Correr Flask: flask --app flaskr --debug run
@@ -30,15 +30,18 @@ def create_app(test_config=None):
 
     # Not find page
     @app.errorhandler(404)
+    @cross_origin()
     def page_not_found(error):
         abort(404, "wrong path")
 
     # Test ping
     @app.get("/ping")
+    @cross_origin()
     def ping():
         return "<p>pong api version: 0.1</p>"
 
     @app.errorhandler(Exception)
+    @cross_origin()
     def handle_exception(e):
         # pass through HTTP errors
         if isinstance(e, HTTPException):
@@ -48,6 +51,7 @@ def create_app(test_config=None):
     # Hacemos los api para cargar, y leer los datos de los archivos que hay en la BD
 
     @app.route('/upload-file', methods=['POST'])
+    @cross_origin()
     def upload_file():
         request_data = request.get_json()
         upload_file_service(request_data)
@@ -55,14 +59,17 @@ def create_app(test_config=None):
         return jsonify(response)
 
     @app.get("/get-files")
+    @cross_origin()
     def get_files():
         return get_files_service()
 
     @app.get("/get-storage")
+    @cross_origin()
     def get_storage():
         return get_storage_service()
 
     @app.get("/get-host")
+    @cross_origin()
     def get_host():
         return get_host_service()
 
